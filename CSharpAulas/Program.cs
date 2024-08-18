8e41a37b232151aa4a5c1d7975a091e118a7d694
Jorge <Jorge@DESKTOP-SM52CUE>
2024-08-18 18:15:02 -0300

    Adicionar arquivos de projeto.

---
+++ CSharpAulas/Program.cs
@@ -0,0 +1,54 @@
+﻿using CSharpAulas.Uteis;
+int op = 1, num1,  num2;
+do
+{
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("""
+        0) SAIR
+        1) SOMAR
+        2) SUBTRAIR
+        3) MULTIPICAR
+        4) DIVIDIR
+        5) FATORAR
+        """);
+    op = Convert.ToInt32(Console.ReadLine());
+
+    switch (op)
+    {
+        case 0:
+            Console.WriteLine("O programa será finalizado");
+            break;
+        case 1:
+            LerDoisNumerosInteiros();
+            Console.WriteLine($"O resultado da soma dos números {num1} e {num2} é: {Calculadora.Soma(num1, num2)}");
+            break;
+        case 2:
+            LerDoisNumerosInteiros();
+            Console.WriteLine($"O resultado da subtração dos números {num1} e {num2} é: {Calculadora.Subtracao(num1, num2)}");
+            break;
+        case 3:
+            LerDoisNumerosInteiros();
+            Console.WriteLine($"O resultado da multiplicação dos números {num1} e {num2} é: {Calculadora.Multiplicacao(num1, num2)}");
+            break;
+        case 4:
+            LerDoisNumerosInteiros();
+            Console.WriteLine($"O resultado da divisão dos números {num1} e {num2} é: {Calculadora.Divisao(num1, num2)}");
+            break;
+        case 5:
+            LerDoisNumerosInteiros();
+            Console.WriteLine($"O resultado da fatoração dos números {num1} é: {Calculadora.Fatorial(num1)}");
+            break;  
+
+
+
+    }
+
+} while (op != 0);
+
+void LerDoisNumerosInteiros()
+{
+    Console.WriteLine("Escreva o primeiro número:");
+    num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Escreva o segundo número:");
+    num2 = int.Parse(Console.ReadLine());
+}
---
+++ CSharpAulas/Uteis/Calculadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAulas.Uteis
+{
+    public class Calculadora
+    {
+        public static int Soma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtracao(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiplicacao(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Divisao(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static int Fatorial(int num)
+        {
+            if( num == 1)
+            {
+                return num;
+            }
+            else
+            {
+                return num * Fatorial(num -1);
+            }
+        }
+    }
+
+}
